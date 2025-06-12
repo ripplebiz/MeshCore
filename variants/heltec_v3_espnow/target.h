c52f39be8bd3895c18ee00a18aeeb2ac6af177ba
Memo <58190287+memo-567@users.noreply.github.com>
2025-06-12 05:38:23 +0000

    Update target.h

--- variants/heltec_v3_espnow/target.h
+++ variants/heltec_v3_espnow/target.h
@@ -3,6 +3,7 @@
 #include <helpers/ESP32Board.h>
 #include <helpers/HeltecV3Board.h>
 #include <helpers/esp32/ESPNOWRadio.h>
+#include <helpers/AutoDiscoverRTCClock.h>
 #include <helpers/SensorManager.h>
 #include <helpers/sensors/EnvironmentSensorManager.h>
 #ifdef DISPLAY_CLASS
@@ -11,7 +12,7 @@
 
 extern HeltecV3Board board;
 extern ESPNOWRadio radio_driver;
-extern ESP32RTCClock rtc_clock;
+extern AutoDiscoverRTCClock rtc_clock;
 extern EnvironmentSensorManager sensors;
 
 #ifdef DISPLAY_CLASS
@@ -23,3 +24,4 @@ uint32_t radio_get_rng_seed();
 void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
 void radio_set_tx_power(uint8_t dbm);
 mesh::LocalIdentity radio_new_identity();
+

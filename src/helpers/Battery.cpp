b9e6d757ef94fc68c362b9bc29d637ddd83f3bba
Rob Loranger <robj@mailbox.org>
2025-06-20 14:27:13 -0700

    fix off by one in batt_percent loop

--- src/helpers/Battery.cpp
+++ src/helpers/Battery.cpp
@@ -4,7 +4,8 @@
 uint16_t batt_percent(uint8_t batt, uint16_t mv) {
     uint16_t last = mv; // set last to mv so it can never start greater than itself
     uint16_t p = 0;
-    for (int i = 0; i < 102; i++) { // always 101 elements, 100 percent and 0
+    // 101 elements including 0 percentile
+    for (int i = 0; i < 101; i++) {
         // if we match the current value or are between the current and last value
         // we return the lower of the the two, giving accuracy +/- 1% soc
         if (mv <= last && mv >= batt_curve[i][batt]) {
